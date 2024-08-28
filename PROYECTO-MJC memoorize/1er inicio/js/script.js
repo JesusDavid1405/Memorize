533da4991cd231d39dde3904407d192a983681b8
Camilo-Charry <katcamcharryperez@gmail.com>
2024-08-28 13:34:56 -0500

    actualice n1

---
+++ PROYECTO-MJC memoorize/1er inicio/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var startButton = document.getElementById("startButton");
+
+    startButton.addEventListener("click", function() {
+        alert("¡Bienvenido a MJC Memorize! Prepárate para ejercitar tu mente.");
+    });
+});
+
