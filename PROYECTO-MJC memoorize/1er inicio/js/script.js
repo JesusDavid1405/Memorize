7d110145c4d5c06777db81c8af260d4629f49057
jesus david fierro rivera <jesusdavidfierrorivera817@gmail.com>
2024-09-02 23:12:32 -0500

    elimine una carpeta que no tenia nada importante(cambio de jesus)

--- PROYECTO-MJC memoorize/1er inicio/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var startButton = document.getElementById("startButton");
-
-    startButton.addEventListener("click", function() {
-        alert("¡Bienvenido a MJC Memorize! Prepárate para ejercitar tu mente.");
-    });
-});
-
