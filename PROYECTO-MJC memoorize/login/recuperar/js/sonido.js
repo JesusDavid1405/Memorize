4ec24d3f1162b4b1ed18826ff4f86ccece6ec9c6
WILLYGONZALE <willianstebangonzalezcortes@gmail.com>
2024-09-06 09:22:09 -0500

    actualizo jesus

---
+++ PROYECTO-MJC memoorize/login/recuperar/js/sonido.js
@@ -0,0 +1,14 @@
+var audio = document.getElementById('miCancion');
+var playButton = document.getElementById('playButton');
+var isPlaying = false
+
+playButton.addEventListener('click', function() {
+    if (isPlaying) {
+        audio.pause();
+        playButton.innerHTML = `<img class="imgSonido" src="../img/icon/noSonido.png" alt="">`;
+    } else {
+        audio.play();
+        playButton.innerHTML =  `<img class="imgSonido" src="../img/icon/sonido.png" alt="">`;
+    }
+    isPlaying = !isPlaying;
+});
