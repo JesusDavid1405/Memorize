a6496b1076dec1748455cd3b81c957b7ffdf0512
jesus david fierro rivera <jesusdavidfierrorivera817@gmail.com>
2024-09-08 16:06:43 -0500

    el menu ya casi por terminarse

--- PROYECTO-MJC memoorize/menu/js/sonido.js
+++ PROYECTO-MJC memoorize/menu/js/sonido.js
@@ -5,10 +5,29 @@ var isPlaying = false
 playButton.addEventListener('click', function() {
     if (isPlaying) {
         audio.pause();
-        playButton.innerHTML = `<img class="imgSonido" src="img/icon/noSonido.png" alt="">`;
+        playButton.innerHTML = `<img class="imgSonido" src="../img/icon/noSonido.png" alt="">`;
     } else {
         audio.play();
-        playButton.innerHTML =  `<img class="imgSonido" src="img/icon/sonido.png" alt="">`;
+        playButton.innerHTML =  `<img class="imgSonido" src="../img/icon/sonido.png" alt="">`;
     }
     isPlaying = !isPlaying;
 });
+
+const volumeControl = document.getElementById('volume');
+let iconoVolumen= document.getElementById("iconVolumen")
+
+function updateVolume() {
+    audio.volume = volumeControl.value;
+    if(audio.volume <= 0){
+        iconoVolumen.innerHTML = `<img class="volumen" src="../img/icon/noVolumen.png" alt=""></img>` 
+    }else{
+          
+        iconoVolumen.innerHTML = `<img class="volumen" src="../img/icon/volumen.png" alt=""></img>`
+    }
+    console.log(audio.volume)
+}
+
+volumeControl.addEventListener('input', updateVolume);
+
+updateVolume();
+
