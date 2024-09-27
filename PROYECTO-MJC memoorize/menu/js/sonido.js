5aed94ad090f52fcefe38651a932f93befc54940
unknown <isacarrera1105@gmail.com>
2024-09-27 02:29:44 -0500

    cambio menu

--- PROYECTO-MJC memoorize/menu/js/sonido.js
+++ PROYECTO-MJC memoorize/menu/js/sonido.js
@@ -31,3 +31,39 @@ volumeControl.addEventListener('input', updateVolume);
 
 updateVolume();
 
+
+let avatarSeleccionado = "";
+function seleccionarAvatar(rutaAvatar) {
+    avatarSeleccionado = rutaAvatar;  
+    document.getElementById('avatarBoton').src = avatarSeleccionado; 
+    document.getElementById('avatarPerfil').src = avatarSeleccionado;  
+}
+function guardarAvatar() {
+    const mensajeModalBody = document.getElementById('mensajeModalBody');
+
+    if (avatarSeleccionado !== "") {
+        localStorage.setItem('avatarGuardado', avatarSeleccionado);
+        
+        mensajeModalBody.innerText = "Avatar guardado correctamente!";
+        const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+        mensajeModal.show();
+    } else {
+        mensajeModalBody.innerText = "Por favor selecciona un avatar primero.";
+        const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+        mensajeModal.show();
+    }
+}
+
+function cargarAvatar() {
+    const avatarGuardado = localStorage.getItem('avatarGuardado');
+    if (avatarGuardado) {
+        document.getElementById('avatarBoton').src = avatarGuardado;  
+        document.getElementById('avatarPerfil').src = avatarGuardado;  
+    }
+}
+
+window.onload = cargarAvatar;
+
+function cerrarSesion() {
+    window.location.href = '../login/index.html'; 
+}
