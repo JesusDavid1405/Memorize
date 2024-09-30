7db986da15139ce621b2278b22c8e0e6ce935010
Camilo-Charry <katcamcharryperez@gmail.com>
2024-09-30 17:39:39 -0500

    Charrytablaclasif

--- PROYECTO-MJC memoorize/tablaclasif/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const closeBtn = document.getElementById('closeBtn');
-    const leaderboard = document.getElementById('leaderboard');
-
-    closeBtn.addEventListener('click', function() {
-        leaderboard.style.display = 'none';
-    });
-});
