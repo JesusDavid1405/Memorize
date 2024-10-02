108233ed8a081873d3561d6721c72febc6d3e6d7
unknown <isacarrera1105@gmail.com>
2024-10-01 23:19:50 -0500

    CAMBIO

--- PROYECTO-MJC memoorize/juegos/quiz/js/bar.js
+++ PROYECTO-MJC memoorize/juegos/quiz/js/bar.js
@@ -1,13 +1,22 @@
 function simulateLoading() {
-    var loadingBar = document.getElementById('loading-bar');
+    var wave1 = document.querySelector('.wave1');
+    var wave2 = document.querySelector('.wave2');
+    var wave3 = document.querySelector('.wave3');
     
-    setTimeout(function() {
-        loadingBar.style.width = '100%';
-        
-        setTimeout(function() {
-            window.location.href = 'index.html';
-        }, 1000);
-    }, 1500);
-}
+    let width = 0;
 
-simulateLoading();
+    var interval = setInterval(function() {
+        width += 1;  
+        wave1.style.width = width + "%";
+        wave2.style.width = width + "%";
+        wave3.style.width = width + "%";
+
+        if (width >= 100) {
+            clearInterval(interval);  
+
+            setTimeout(function() {
+                window.location.href = 'index.html'; 
+            }, 1000);
+        }
+    }, 30); 
+}
