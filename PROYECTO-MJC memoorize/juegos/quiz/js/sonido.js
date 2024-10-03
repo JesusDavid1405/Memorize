4eb5e6881feb7a39397b2cf601ffcc2c77acf9af
unknown <isacarrera1105@gmail.com>
2024-10-02 22:45:08 -0500

    arreglo

--- PROYECTO-MJC memoorize/juegos/quiz/js/sonido.js
+++ PROYECTO-MJC memoorize/juegos/quiz/js/sonido.js
@@ -5,10 +5,10 @@ var isPlaying = false
 playButton.addEventListener('click', function() {
     if (isPlaying) {
         audio.pause();
-        playButton.innerHTML = `<img class="imgSonido" src="../../img/icon/noSonido.png" alt="">`;
+        playButton.innerHTML = `<img class="imgSonido" src="../img/icon/noSonido.png" alt="">`;
     } else {
         audio.play();
-        playButton.innerHTML =  `<img class="imgSonido" src="../../img/icon/sonido.png" alt="">`;
+        playButton.innerHTML =  `<img class="imgSonido" src="../img/icon/sonido.png" alt="">`;
     }
     isPlaying = !isPlaying;
 });
@@ -19,10 +19,10 @@ let iconoVolumen= document.getElementById("iconVolumen")
 function updateVolume() {
     audio.volume = volumeControl.value;
     if(audio.volume <= 0){
-        iconoVolumen.innerHTML = `<img class="volumen" src="../../img/icon/noVolumen.png" alt=""></img>` 
+        iconoVolumen.innerHTML = `<img class="volumen" src="../img/icon/noVolumen.png" alt=""></img>` 
     }else{
           
-        iconoVolumen.innerHTML = `<img class="volumen" src="../../img/icon/volumen.png" alt=""></img>`
+        iconoVolumen.innerHTML = `<img class="volumen" src="../img/icon/volumen.png" alt=""></img>`
     }
     console.log(audio.volume)
 }
@@ -30,4 +30,41 @@ function updateVolume() {
 volumeControl.addEventListener('input', updateVolume);
 
 updateVolume();
+
+
+let avatarSeleccionado = '../img/icon/usuario.png';
+function seleccionarAvatar(rutaAvatar) {
+    avatarSeleccionado = rutaAvatar;  
+    document.getElementById('avatarBoton').src = avatarSeleccionado; 
+    document.getElementById('avatarPerfil').src = avatarSeleccionado;  
+}
+function guardarAvatar() {
+    const mensajeModalBody = document.getElementById('mensajeModalBody');
+
+    if (avatarSeleccionado !== "") {
+        localStorage.setItem('avatarGuardado', avatarSeleccionado);
+        
+        mensajeModalBody.innerText = "Avatar guardado correctamente!";
+        const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+        mensajeModal.show();
+    } else {
+        mensajeModalBody.innerText = "Por favor selecciona un avatar primero.";
+        const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+        mensajeModal.show();
+    }
+}
+
+function cargarAvatar() {
+    const avatarGuardado = localStorage.getItem('avatarGuardado');
+    if (avatarGuardado) {
+        document.getElementById('avatarBoton').src = avatarGuardado;  
+        document.getElementById('avatarPerfil').src = avatarGuardado;  
+    }
+}
+
+window.onload = cargarAvatar;
+
+function cerrarSesion() {
+    window.location.href = '../login/index.html'; 
+}
 
