0503fcf1aac7c68bd8786396a90c177c75c549ed
keinernarvaez <ckeinercano@gmail.com>
2024-10-03 16:25:30 -0500

    multijugador

--- PROYECTO-MJC memoorize/multijugador/sala/js/main.js
+++ PROYECTO-MJC memoorize/multijugador/sala/js/main.js
@@ -1,35 +1,35 @@
 const user = localStorage.getItem('user');
+const codigoSala= localStorage.getItem('codigo');
+
+let modalError= new bootstrap.Modal(document.getElementById('msgError'));
+let modalDisplay= document.querySelector('.modal-body');
 
 let display = document.getElementById('result');
 
-let codigoSala=generateCode();
+
 console.log(codigoSala)
 
 if (user == 1) {
     display.innerHTML = `
-    <div class="sala">
-        <div class="col-12">
-            <img class="logo" src="../../img/logo/multijugador.png" alt="...">
-            <div class="codigoSala">
-                <input class="input" type="number" id="codigoSala" maxlength="5" minlength="5" placeholder="codigo de la sala">
-            </div>
-            <div class="btnComparar">
-                <button id="comparar">Ingresar a la Sala</button>
-            </div>
-        </div>
+    <div class="col-12">
+        <img class="logo" src="../../img/logo/multijugador.png" alt="...">
+    <div class="codigoSala">
+        <input class="input" type="text" id="nombreSala" placeholder="nombre de la sala">
     </div>
+    <div class="btnComparar">
+        <button id="crearSala">Crear Sala</button>
+    </div>
+        
     `;
 } else if (user == 2) {
     display.innerHTML = `
-    <div class="sala">
-        <div class="col-12">
-            <img class="logo" src="../../img/logo/multijugador.png" alt="...">
-            <div class="codigoSala">
-                <input class="input" type="number" id="codigoSala" maxlength="5" minlength="5" placeholder="codigo de la sala">
-            </div>
-            <div class="btnComparar">
-                <button id="comparar">Ingresar a la Sala</button>
-            </div>
+    <div class="col-12">
+        <img class="logo" src="../../img/logo/multijugador.png" alt="...">
+        <div class="codigoSala">
+            <input class="input" type="number" id="nombreSala" maxlength="5" minlength="5" placeholder="codigo de la sala">
+        </div>
+        <div class="btnComparar">
+            <button id="comparar">Ingresar a la Sala</button>
         </div>
     </div>
     `;
@@ -37,31 +37,47 @@ if (user == 1) {
     display.innerHTML = 'Error: no se ha especificado un usuario.';
 }
 
+/*obtener nombre de la sala*/
+
+let btnCrearSala = document.getElementById('crearSala');
+
+
+btnCrearSala.addEventListener('click',function(){
+    let nombreSala= document.getElementById('nombreSala').value;
+    console.log(nombreSala)
+    if(nombreSala == null){
+        modalDisplay.innerHTML=`
+        por favor ingrese un nombre
+        `;
+        modalError.show()
+    }else{
+        display.innerHTML=`
+        <div class="sala">
+            <div class="header">
+                <h1>${nombreSala} </h1>
+            </div>
+            <div class="body-multijugador">
+                
+            </div>
+        </div>
+        `;  
+    }
+    
+})
+
+/*unirse*/
+
 let btnComparar=document.getElementById('comparar');
+let codigoUser=document.getElementById('codigoSala').value;
 
 btnComparar.addEventListener('click',function() {
-    let codigoUser=document.getElementById('codigoSala').value;
-    let modalError= new bootstrap.Modal(document.getElementById('msgError'));
-    let modalDisplay= document.querySelector('.modal-body');
-
     if(codigoUser == codigoSala){
         display.innerHTML=`
         se ingreso el codigo correcto
         `;
     }else{
         modalDisplay.innerHTML=`el codigo que ingresaste no pertenece a ninguna sala`;
+        modalError.show()
     }
-    modalError.show()
-})
     
-
-function generateCode(){
-    let codigo;
-
-    // Generar un número aleatorio entre 10000 y 99999
-    let generate = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
-
-    codigo = generate;
-
-    return codigo;
-}
+})
