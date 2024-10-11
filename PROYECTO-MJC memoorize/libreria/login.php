572e12e5488b24bae9d49619a745a818e86a2506
Camilo-Charry <katcamcharryperez@gmail.com>
2024-10-11 09:36:55 -0500

    yisus

--- PROYECTO-MJC memoorize/libreria/login.php
+++ PROYECTO-MJC memoorize/libreria/login.php
@@ -12,18 +12,28 @@
         "isabellacarrera@gmail.com",
         "isa123"
     );
+    $usuario3= new Usuario(
+        "cachape",
+        "cachape64@gmail.com",
+        "charry123"
+    );
 
     $response=[];
 
     $response[] = [
         'nickName' => $usuario1->getNickName(),
-        'correo' => $usuario1->getCorreo(),
+        'correo' => $usuario1->getCorreo(),     
         'contraseña' => $usuario1->getContraseña(),
     ];
     $response[] = [
         'nickName' => $usuario2->getNickName(),
         'correo' => $usuario2->getCorreo(),
         'contraseña' => $usuario2->getContraseña(),
+    ];
+    $response[] = [
+        'nickName' => $usuario3->getNickName(),
+        'correo' => $usuario3->getCorreo(),
+        'contraseña' => $usuario3->getContraseña(),
     ];
 
     header('content-Type: application/json');
