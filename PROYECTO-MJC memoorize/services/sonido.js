ea1bb8d7b285f9d9c0c8a435f8590ea7c73cd967
CritianJPalma <cristianspalmas@gmail.com>
2024-10-19 11:47:56 -0500

    cambios

---
+++ PROYECTO-MJC memoorize/services/sonido.js
@@ -0,0 +1,31 @@
+var audio = document.getElementById('miCancion');
+var playButton = document.getElementById('playButton');
+var isPlaying = false
+
+playButton.addEventListener('click', function() {
+    if (isPlaying) {
+        audio.pause();
+        playButton.innerHTML = `<img class="imgSonido" src="../img/icon/noSonido.png" alt="">`;
+    } else {
+        audio.play();
+        playButton.innerHTML =  `<img class="imgSonido" src="../img/icon/sonido.png" alt="">`;
+    }
+    isPlaying = !isPlaying;
+});
+const sound = new Audio('audio/notification-sound-7062.mp3');
+
+
+document.querySelector('.button2').addEventListener('mouseenter', () => {
+    sound.currentTime = 0;
+    sound.play();
+});
+
+document.querySelector('.logo').addEventListener('mouseenter', () => {
+    sound.currentTime = 0;
+    sound.play();
+});
+
+document.querySelector('.imgSonido').addEventListener('mouseenter', () => {
+    sound.currentTime = 0;
+    sound.play();
+});
