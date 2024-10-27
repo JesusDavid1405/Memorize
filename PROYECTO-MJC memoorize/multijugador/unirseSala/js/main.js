1d8c5958a2e42a9ccf96b41e11b663396c67f3da
isacarrera <isacarrera1105@gmail.com>
2024-10-27 02:13:20 -0500

    cc

--- PROYECTO-MJC memoorize/multijugador/unirseSala/js/main.js
+++ PROYECTO-MJC memoorize/multijugador/unirseSala/js/main.js
@@ -1,27 +1,32 @@
-let btnComparar=document.getElementById('comparar');
 
-const codigoSala=localStorage.getItem('codigo')
-console.log(codigoSala)
+      localStorage.setItem('codigo', '1234'); 
+      localStorage.setItem('rondas', 2); 
 
-btnComparar.addEventListener('click',function() {
-    let codigoUser=document.getElementById('codigoSala').value.trim();  
-    let modalError= new bootstrap.Modal(document.getElementById('msgError'));
-    let modalDisplay= document.querySelector('.modal-body');
+      let btnComparar = document.getElementById('comparar');
+      const codigoSala = localStorage.getItem('codigo');
 
-    if(codigoUser === ""){
-        modalDisplay.innerHTML=`no haz ingresado ningun codigo, por favor ingresa uno`;
-        modalError.show()
-    }else if(codigoUser === codigoSala){
+      btnComparar.addEventListener('click', function() {
+          let codigoUser = document.getElementById('codigoSala').value.trim();  
+          let modalError = new bootstrap.Modal(document.getElementById('msgError'));
+          let modalDisplay = document.querySelector('#msgError .modal-body');
+          if (codigoUser === "") {
+              modalDisplay.innerHTML = `No has ingresado ningún código, por favor ingresa uno.`;
+              modalError.show();
+          } 
+          // Verificamos si el código es correcto
+          else if (codigoUser === codigoSala) {
+              let rondas = localStorage.getItem('rondas');
+              iniciarJuego(rondas);
+          } 
+          // Si el código es incorrecto
+          else {
+              modalDisplay.innerHTML = `El código que ingresaste no pertenece a ninguna sala.`;
+              modalError.show();
+          }
+          localStorage.setItem('rol', 2);
+      });
 
-        let rondas = localStorage.getItem('rondas'); // Recuperar el número de rondas
-        iniciarJuego(rondas); // Llama a tu función de iniciar el juego
-    }else{
-        modalDisplay.innerHTML=`el codigo que ingresaste no pertenece a ninguna sala`;
-        modalError.show()
-    }
-    localStorage.setItem('rol',2)
-})
-function iniciarJuego(rondas) {
-    console.log(`El juego comenzará con ${rondas} rondas.`);
-    // Aquí debes implementar la lógica del juego
-}
+      function iniciarJuego(rondas) {
+          console.log(`El juego comenzará con ${rondas} rondas.`);
+          window.location.href = '../crearSala/index.html';
+      }
