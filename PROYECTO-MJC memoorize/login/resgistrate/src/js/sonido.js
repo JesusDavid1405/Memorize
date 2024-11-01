086e9b9d1b835effbe03d7deeebc499d32f7393d
unknown <isacarrera1105@gmail.com>
2024-11-01 01:51:41 -0500

    creo

--- PROYECTO-MJC memoorize/login/resgistrate/src/js/sonido.js
+++ PROYECTO-MJC memoorize/login/resgistrate/src/js/sonido.js
@@ -60,15 +60,3 @@ document.querySelector('a').addEventListener('mouseenter', () => {
     sound.currentTime = 0;
     sound.play();
 });
-document.getElementById("togglePassword").addEventListener("click", function() {
-    const passwordField = document.getElementById("contraseñaLogin");
-    const eyeIcon = document.getElementById("eyeIcon");
-
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        eyeIcon.src = "../../img/icon/password.png"; // Cambia al icono de ojo abierto
-    } else {
-        passwordField.type = "password";
-        eyeIcon.src = "../../img/icon/candado1.png"; // Cambia al icono de contraseña cerrado
-    }
-});
