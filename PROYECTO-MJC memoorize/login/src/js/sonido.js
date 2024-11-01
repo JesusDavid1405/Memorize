f5a1e45220dccf2d30bd4f2c82cd909e62441f48
unknown <isacarrera1105@gmail.com>
2024-11-01 01:24:51 -0500

    m rindo

--- PROYECTO-MJC memoorize/login/src/js/sonido.js
+++ PROYECTO-MJC memoorize/login/src/js/sonido.js
@@ -56,18 +56,3 @@ document.querySelector('a').addEventListener('mouseenter', () => {
     sound.currentTime = 0;
     sound.play();
 });
-<<<<<<< HEAD
-document.getElementById("togglePassword").addEventListener("click", function() {
-    const passwordField = document.getElementById("contraseñaLogin");
-    const eyeIcon = document.getElementById("eyeIcon");
-
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        eyeIcon.src = "../img/icon/password.png"; // Cambia al icono de ojo abierto
-    } else {
-        passwordField.type = "password";
-        eyeIcon.src = "../img/icon/candado1.png"; // Cambia al icono de contraseña cerrado
-    }
-});
-=======
->>>>>>> 4256f72e766f7249d879250eddccbff65d440012
