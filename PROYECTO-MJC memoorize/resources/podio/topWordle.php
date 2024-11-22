311acd9df139eec1e72e6e344350742498ffe9a9
jesus david fierro rivera <jesusdavidfierrorivera817@gmail.com>
2024-11-22 11:50:09 -0500

    podio en proceso

--- PROYECTO-MJC memoorize/resources/podio/topWordle.php
+++ PROYECTO-MJC memoorize/resources/podio/topWordle.php
@@ -1,76 +1,84 @@
 <?php
 
-include('../conexion/conexion.php');
+    include('../conexion/conexion.php');
 
-session_start();
+    session_start();
 
-$response = [];
+    $response = [];
 
-$database = new Database();
-$conn = $database->connect();
+    $input = file_get_contents('php://input');
+    $data = json_decode($input, true);
 
-if ($conn) {
-    $query = "
-    SELECT 
-        hn.id AS historialId,
-        u.nickName AS usuario,
-        a.nombre AS avatarNombre,
-        a.imagen AS avatarImagen,
-        j.nombre AS juego,
-        n.numero AS nivel,
-        hn.puntos,
-        hn.tiempo,
-        hn.monedas,
-        hn.estadoNivel
-    FROM 
-        historialNiveles hn
-    JOIN 
-        usuarios u ON hn.usuarioId = u.id
-    JOIN 
-        juegos j ON hn.juegoId = j.id
-    JOIN 
-        avatares a ON u.avatarId = a.id
-    JOIN
-        niveles n ON hn.nivelId= n.id
-    WHERE 
-        hn.estadoNivel = 1 
-        AND j.id = IFNULL(?, 2) -- Si no se proporciona un ID de juego, usa el juego con ID = 1
-    ORDER BY 
-        hn.puntos DESC
-    LIMIT 10;
-    ";
 
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("i", $juego);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    if ($data && isset($data['id'])) {
+        $juego=$data['id'];
 
-    if ($result) {
+        $database = new Database();
+    $conn = $database->connect();
 
-        while ($row = $result->fetch_assoc()) {
-            $response[]=[
-                'avatar' => $row['avatarImagen'],
-                'nickName' => $row['usuario'],
-                'puntos'=> $row['puntos'],
-                'tiempo'=>$row['tiempo'],
-                'nivel' => $row['nivel']
+    if ($conn) {
+        $query = "
+        SELECT 
+            hn.id AS historialId,
+            u.nickName AS usuario,
+            a.nombre AS avatarNombre,
+            a.imagen AS avatarImagen,
+            j.nombre AS juego,
+            n.numero AS nivel,
+            hn.puntos,
+            hn.tiempo,
+            hn.monedas,
+            hn.estadoNivel
+        FROM 
+            historialNiveles hn
+        JOIN 
+            usuarios u ON hn.usuarioId = u.id
+        JOIN 
+            juegos j ON hn.juegoId = j.id
+        JOIN 
+            avatares a ON u.avatarId = a.id
+        JOIN
+            niveles n ON hn.nivelId= n.id
+        WHERE 
+            hn.estadoNivel = 1 
+            AND j.id = IFNULL(?, 2) -- Si no se proporciona un ID de juego, usa el juego con ID = 1
+        ORDER BY 
+            hn.puntos DESC
+        LIMIT 10;
+        ";
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("i", $juego);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result) {
+
+            while ($row = $result->fetch_assoc()) {
+                $response[]=[
+                    'avatar' => $row['avatarImagen'],
+                    'nickName' => $row['usuario'],
+                    'puntos'=> $row['puntos'],
+                    'tiempo'=>$row['tiempo'],
+                    'nivel' => $row['nivel']
+                ];
+            }
+        } else {
+
+            $response = [
+                'status' => 'FALSE',
+                'mensaje' => 'No hay avatares disponibles para mostrar.'
             ];
-        }
-    } else {
 
-        $response = [
-            'status' => 'FALSE',
-            'mensaje' => 'No hay avatares disponibles para mostrar.'
-        ];
+        }
+
+        $database->disconnect();
+    }
 
     }
 
-    $database->disconnect();
-}
-
-
-header('Content-Type: application/json');
-echo json_encode($response);
+    header('Content-Type: application/json');
+    echo json_encode($response);
 
 
 ?>
--- PROYECTO-MJC memoorize/services/podio/top.js
+++ PROYECTO-MJC memoorize/services/podio/top.js
@@ -1,5 +1,5 @@
 let displayTop = document.getElementById('topGlobal');
-let displayJuegosTop=
+let displayJuegosTop= document.getElementById('juegosTop');
 
 fetch('../resources/juegos/juegos.php',{
     method: 'POST',
@@ -13,36 +13,62 @@ fetch('../resources/juegos/juegos.php',{
 .then(response => response.json())
 .then(data => {
     data.forEach(juegos =>{
-        
+        // displayJuegosTop.innerHTML+=`
+        // <ul>
+        //     <li class="dropdown">
+        //         <a href="javascript:void(0)">${juegos.nombre}</a>
+        //         <ul class="dropdown-content">
+        //             <li><a href="#">Action</a></li>
+        //             <li><a href="#">Another Action</a></li>
+        //             <li><a href="#">Something Else</a></li>
+        //         </ul>
+        //     </li>
+        // </ul>
+        // `;
+        displayJuegosTop.innerHTML+=`
+            <li class="juegos-opcion" data-id="${juegos.id}">${juegos.nombre}</li>
+        `;
     })
 
-    fetch('../resources/podio/topWordle.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            id: ''
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
-            displayTop.innerHTML+=`
-            <tr>
-                <td>
-                    <div class="player-info">
-                        <img src="../img/${element.avatar}" alt="Avatar 1" class="avatar">
-                        <span>${element.nickName}</span>
-                    </div>
-                </td>
-                <td>${element.puntos}</td>
-                <td>${element.tiempo}</td>
-                <td>${element.nivel}</td>
-            </tr>
-            `;
+    let juegosOpcion= document.querySelectorAll('.juegos-opcion');
+
+    juegosOpcion.forEach(opcion =>{
+        opcion.addEventListener('click',(event)=>{
+            let juegoId = event.target.getAttribute('data-id');
+
+            displayTop.innerHTML='';
+
+            fetch('../resources/podio/topWordle.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: juegoId
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(element => {
+                    displayTop.innerHTML+=`
+                    <tr>
+                        <td>
+                            <div class="player-info">
+                                <img src="../img/${element.avatar}" alt="Avatar 1" class="avatar">
+                                <span>${element.nickName}</span>
+                            </div>
+                        </td>
+                        <td>${element.puntos}</td>
+                        <td>${element.tiempo}</td>
+                        <td>${element.nivel}</td>
+                    </tr>
+                    `;
+                    
+                });
+            })
+            .catch(error => console.error('Error:', error));
+            
         });
-    })
-    .catch(error => console.error('Error:', error));
+    });
 })
 .catch(error => console.error('Error:', error));   
