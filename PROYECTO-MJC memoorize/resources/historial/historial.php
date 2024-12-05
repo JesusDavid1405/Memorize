2bcd75b9286cee4290fa638dde2aa051156b0e63
jesus david fierro rivera <jesusdavidfierrorivera817@gmail.com>
2024-12-04 19:24:43 -0500

    historial

--- PROYECTO-MJC memoorize/resources/historial/historial.php
+++ PROYECTO-MJC memoorize/resources/historial/historial.php
@@ -1,77 +1,105 @@
 <?php
-  include('../conexion/conexion.php');
+    include('../conexion/conexion.php');
 
-session_start();
+    session_start();
 
     $response= [];
-    $historial= $_SESSION['id'];
-        $database = new Database();
-        $conn = $database->connect();
-        if($conn){
-            $query="
+    $usuarioId= $_SESSION['id'];
+
+    $database = new Database();
+    $conn = $database->connect();
+
+    if($conn){
+        $query="
         SELECT 
-            hn.usuarioId AS usuario_id,
-            u.nickName AS usuario_nickname,
-            hn.juegoId AS juego_id,
-            j.nombre AS juego_nombre,
-            hn.nivelId AS nivel_id,
-            n.numero AS nivel_numero,
+            j.nombre AS juego,
+            hn.juegoId AS nivelSalaId,
+            n.nombre AS nombre,
             hn.puntos AS puntos,
             hn.monedas AS monedas,
             hn.tiempo AS tiempo,
-            hn.estadoNivel AS estado,
-            hn.fechaInicio as fecha,
-            'niveles' AS modalidad
+            hn.fechaInicio AS fecha,
+            'niveles' AS modalidad,
+            CASE 
+                WHEN hn.estadoNivel = 1 THEN 'Ganaste'
+                ELSE 'Derrota'
+            END AS estado
         FROM 
             historialNiveles hn
         JOIN 
             usuarios u ON hn.usuarioId = u.id
         JOIN 
-            juegos j ON hn.juegoId = j.id
-        JOIN 
             niveles n ON hn.nivelId = n.id
-        WHERE u.id= 1
+        JOIN 
+            juegos j ON hn.juegoId = j.id
+        WHERE 
+            u.id = 1
 
         UNION ALL
 
         SELECT 
-            hs.participanteId AS usuario_id,
-            u.nickName AS usuario_nickname,
-            NULL AS juego_id,  -- No se utiliza juegoId en multijugador
-            NULL AS juego_nombre,
-            NULL AS nivel_id,  -- No aplica nivel en multijugador
-            NULL AS nivel_numero,
+            j.nombre AS juego,
+            hs.salaId AS nivelSalaId,
+            s.nombre AS nombre,
             hs.puntosTotal AS puntos,
-            NULL AS monedas,  -- Las monedas no se consideran en multijugador
+            hs.monedas AS monedas,  -- No aplica monedas en historialSala
             hs.tiempoPromedio AS tiempo,
-            hs.podioFinal AS estado,
-            hs.fechaInicio as fecha,
-            'multijugador' AS modalidad
+            hs.fechaInicio AS fecha,
+            'multijugador' AS modalidad,
+            CASE 
+                WHEN hs.podioFinal BETWEEN 1 AND 3 THEN 'Ganaste'
+                ELSE 'Derrota'
+            END AS estado
         FROM 
             historialSala hs
         JOIN 
             usuarios u ON hs.participanteId = u.id
-        WHERE u.id= 1
-        ORDER BY fecha DESC;
+        JOIN 
+            juegos j ON hs.juegoId = j.id
+        JOIN 
+            sala s ON hs.salaId = s.id
+        WHERE 
+            u.id = 1
+        ORDER BY 
+            fecha DESC
+        LIMIT 0, 25;
         ";
-            $stmt= $conn->prepare($query);
-            $stmt->bind_param("isiisi",
-                $creadorSala,
-                $nombreSala,
-                $codigoSala,
-                $capacidadSala,
-                $dificultadSala,
-                $rondasSala
-            );
-            $stmt->execute();
 
-            $response=[
-                'status' => 'true',
-                'mensaje'=>'historial guardado exitosamente'
-            ];
-        }else{
-            $response=['mensaje'=>"error al guardar historial"];
-        }
+        $stmt= $conn->prepare($query);
+        $stmt->bind_param("ii",
+            $usuarioId,
+            $usuarioId
+        );
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+            if ($result) {
+                while ($row = $result->fetch_assoc()) {
+                    $response+=[
+                        'juego' => $row['juego'],
+                        'nivelSalaId' => $row['nivelSalaId'],
+                        'nombre' => $row['nombre'],
+                        'puntos' => $row['puntos'],
+                        'tiempo' => $row['tiempo'],
+                        'monedas' => $row['monedas'],
+                        'fecha' => $row['fecha'],
+                        'modalidad' => $row['modalidad'],
+                        'estado' => $row['estado']
+                    ];
+                }
+            } else {
+    
+                $response = [
+                    'status' => 'FALSE',
+                    'mensaje' => 'no has jugado ninguna partida.'
+                ];
+    
+            }
+
+    }else{
+        $response=['mensaje'=>"error al guardar historial"];
+    }
+
     header('Content-Type: application/json');
     echo json_encode($response);
 ?>
